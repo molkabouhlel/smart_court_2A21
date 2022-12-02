259e21bf35f67ac1aea24c37768530ae4ef3774e
chedli <chedhly.ghorbel@esprit.tn>
2022-12-02 11:48:04 +0100

    chedly integ
    Signed-off-by: chedli <chedhly.ghorbel@esprit.tn>

--- connection.cpp
+++ connection.cpp
@@ -1,24 +1,18 @@
-
 #include "connection.h"
-//test git
+
 Connection::Connection()
 {
 
 }
 
 bool Connection::createconnect()
-{bool test=false;
+{
+   bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
 db.setDatabaseName("Source_Projet2A");
-db.setUserName("MedAmine");//inserer nom de l'utilisateur
-db.setPassword("amino159753");//inserer mot de passe de cet utilisateur
+db.setUserName("chedli");//inserer nom de l'utilisateur
+db.setPassword("esprit18");//inserer mot de passe de cet utilisateur
 if (db.open())
 test=true;
-
-
-
-
-
-    return  test;
+   return  test;
 }
-
